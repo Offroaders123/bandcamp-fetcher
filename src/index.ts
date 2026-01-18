093e3fe3f180abfc2c3f4929c3f24d3e80707b8d
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2026-01-18 00:02:33 -0800

    Test File
    Okay, this isn't a "test" yet, more just a file that I can run while demoing this out to make sure it works. Once things are actually implemented, it will probably be turned into a test test.
    
    Clumpy clumpy o

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-const demo = await fetcher("rbnaodn");
-console.log(demo);
-
 export async function fetcher(artist: string): Promise<string> {
   const response: Response = await fetch(`https://${artist}.bandcamp.com/`);
   const html: string = await response.text();
---
+++ test/index.test.ts
@@ -0,0 +1,4 @@
+import { fetcher } from "../src/index.js";
+
+const demo = await fetcher("rbnaodn");
+console.log(demo);
