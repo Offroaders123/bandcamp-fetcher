f84856820f2f7a082d16cc996273eeed1b8146fb
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2026-01-19 20:28:47 -0800

    No Test Fetcher
    Okay, it's a slower test suite, but now it will be accurate to what is fetched from the network. I hope I don't get throttled by doing this. Will learn I guess.
    
    Not that this code runs a whole bunch, more just that I want to be testing on the correct HTML. Looks like it is the same as the incognito version though, so maybe I can just test on that, and make my code work for that instead.

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,17 +1,8 @@
 import { HTMLElement } from "node-html-parser";
-import { getRootPath, parseHTML, releases } from "../src/index.js";
-
-/**
- * This is used as a demo instead of `fetcher()`.
- */
-async function fetchHTMLTest(_path: string): Promise<string> {
-  const { readFile } = await import("node:fs/promises");
-  const html: string = await readFile("./test/fixtures/index.html", "utf-8");
-  return html;
-}
+import { fetchHTML, getRootPath, parseHTML, releases } from "../src/index.js";
 
 const rootPath: string = getRootPath("rBnaodn");
-const html: string = await fetchHTMLTest(rootPath);
+const html: string = await fetchHTML(rootPath);
 const root: HTMLElement = parseHTML(html);
 const releasePaths: string[] = releases(root);
 console.log(releasePaths);
