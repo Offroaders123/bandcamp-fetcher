e2212bc13a7f44a37dc7c740ae8eb21e68826f86
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2026-01-19 22:48:21 -0800

    Save Data Locally
    Okay, yes.
    
    Listening to "F* Webview Psychosis".
    
    It was a demo from today, essentially. Going to write some classic style sounding guitar songs, where there are solos and that sort of thing again. Vibe-first.

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,11 @@
+import { writeFile } from "node:fs/promises";
 import { fetchReleases, type Release } from "../src/index.js";
 
 const artist = "rBnaodn";
 const releases: Release[] = await fetchReleases(artist);
 
-for (const release of releases) {
-  console.log(release);
-}
+// for (const release of releases) {
+//   console.log(release);
+// }
+
+await writeFile("./release-data.json", JSON.stringify(releases, null, 2), "utf-8");
